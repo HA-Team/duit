89c4e64a712fbd2c11c6d168dfa3bd1425b707a4
Antonio Aznarez <antonioaznarez@ha-team.co>
2017-08-23 14:54:54 -0300

    Minification and deploy

--- scripts/meteor-runtime-config.js
+++ scripts/meteor-runtime-config.js
@@ -1,5 +1,15 @@
-__meteor_runtime_config__ = {
-  "meteorEnv": {},
-  "DDP_DEFAULT_CONNECTION_URL": "http://localhost:3000",
-  "ROOT_URL": "http://localhost:8080"
-};
+__meteor_runtime_config__ = (() => {
+  if (window.location.href.indexOf('localhost') === -1) {
+    return {
+      "meteorEnv": {NODE_ENV: 'production'},
+      "DDP_DEFAULT_CONNECTION_URL": "http://localhost:3000",
+      "ROOT_URL": "http://duitpropiedades.com.ar"
+    }
+  } else {
+    return {
+      "meteorEnv": {},
+      "DDP_DEFAULT_CONNECTION_URL": "http://localhost:3000",
+      "ROOT_URL": "http://localhost:8080"
+    }
+  }
+})();
