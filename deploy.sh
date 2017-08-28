2a27c93470882148ef9c9ce82203f6ed89f3c251
Antonio Aznarez <antonioaznarez@ha-team.co>
2017-08-27 21:38:36 -0300

    Minification and deploy

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 #First go to master and update branch
 git checkout master
 git pull origin master
-#Minify Meteor bundle, ui functions and tokkoapi
-uglifyjs scripts/meteor-client.bundle.js -o scripts/meteor-client.bundle.js
-uglifyjs scripts/uiFunctions.js -o scripts/uiFunctions.js
-uglifyjs scripts/tokkoApi.js -o scripts/tokkoApi.js
+# Minify Meteor bundle, ui functions and tokkoapi
+# Require babel minify --> npm install babel-minify -g
+minify scripts/meteor-client.bundle.js -d scripts/
+minify scripts/uiFunctions.js -d scripts/
+minify scripts/tokkoApi.js -d scripts/
 #commit and push
 git add -u
 git commit -m 'Minification and deploy'
