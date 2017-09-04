1aaaff7c0bcf8dade05891b27feb082073137fbf
Antonio Aznarez <antonioaznarez@ha-team.co>
2017-09-04 11:22:47 -0300

    production and development environments setted

--- scripts/meteor-runtime-config.js
+++ scripts/meteor-runtime-config.js
@@ -2,14 +2,14 @@ __meteor_runtime_config__ = (() => {
   if (window.location.href.indexOf('localhost') === -1) {
     return {
       "meteorEnv": {NODE_ENV: 'production'},
-      "DDP_DEFAULT_CONNECTION_URL": "http://localhost:3000",
-      "ROOT_URL": "http://duitpropiedades.com.ar"
+      "DDP_DEFAULT_CONNECTION_URL": "http://backend.duitpropiedades.com.ar",
+      "ROOT_URL": "http://duit.netlify.com",
     }
   } else {
     return {
-      "meteorEnv": {},
+      "meteorEnv": {NODE_ENV: 'development'},
       "DDP_DEFAULT_CONNECTION_URL": "http://localhost:3000",
-      "ROOT_URL": "http://localhost:8080"
+      "ROOT_URL": "http://localhost:8080",
     }
   }
 })();
