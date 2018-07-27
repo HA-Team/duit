dcd9e45c81e3e77fcba4c86691ad0c232171972b
Antonio Aznarez <aaznarez@mbp-be-0004.fritz.box>
2018-07-27 09:04:04 +0200

    added two new asesors

--- data/agents.js
+++ data/agents.js
@@ -1,71 +1,101 @@
-const agents = [{
-  name: 'Agustín Aznarez',
-  position: 'Asesor',
-  phone: '(351) 3102556',
-  email: 'agustin@duitpropiedades.com.ar',
-  facebook: 'https://www.facebook.com/agustin.aznarez',
-  twitter: '',
-  linkedin: '',
-  instagram: '',
-  pic: '/images/team/agustin-aznarez-profile.jpg'
-}, {
-  name: 'Martín Paradelo',
-  position: 'Asesor',
-  phone: '(351) 2463606',
-  email: 'mparadelo@duitpropiedades.com.ar',
-  facebook: 'https://www.facebook.com/martin.paradelo.7',
-  twitter: '',
-  linkedin: '',
-  instagram: 'https://www.instagram.com/martinparadelo/',
-  pic: '/images/team/martin-paradelo-profile.jpg'
-}, {
-  name: 'Daniel Ganim',
-  position: 'Asesor',
-  phone: '(351) 5647780',
-  email: 'danielganim@duitpropiedades.com.ar',
-  facebook: ' ',
-  twitter: '',
-  linkedin: '',
-  instagram: '',
-  pic: '/images/team/daniel-ganim-profile.jpg'
-}, {
-  name: 'Fernando Gavier',
-  position: 'Asesor',
-  phone: '(0351) 155145491',
-  email: 'fgavier@duitpropiedades.com.ar',
-  facebook: 'https://www.facebook.com/fngavier',
-  twitter: 'https://twitter.com/fngavier',
-  linkedin: '',
-  instagram: '',
-  pic: '/images/team/fernando-gavier-profile.jpg'
-}, {
-  name: 'Hugo Miranda',
-  position: 'Asesor',
-  phone: '(351) 2462630',
-  email: 'hmiranda@duitpropiedades.com.ar',
-  facebook: ' ',
-  twitter: '',
-  linkedin: '',
-  instagram: '',
-  pic: '/images/team/hugo-miranda-profile.jpg'
-}, {
-  name: 'Severo Sosa',
-  position: 'Asesor',
-  phone: '(351) 5648467',
-  email: 'severo@duitpropiedades.com.ar',
-  facebook: 'https://www.facebook.com/sapo.sosa',
-  twitter: 'https://twitter.com/severososa',
-  linkedin: 'https://www.linkedin.com/in/severo-sosa-barreneche-590a3b1b/',
-  instagram: 'http://www.instagram.com/sapososa/',
-  pic: '/images/team/severo-sosa-profile.jpg'
-}, {
-  name: 'Teresita Sciortino',
-  position: 'Secretaria Administración',
-  phone: '(351) 6840250',
-  email: 'teresita@duitpropiedades.com.ar',
-  facebook: 's',
-  twitter: '',
-  linkedin: '',
-  instagram: '',
-  pic: '/images/team/recepcion-profile.jpg'
-}]
+const agents = [
+  {
+    name: 'Agustín Aznarez',
+    position: 'Asesor',
+    phone: '(351) 3102556',
+    email: 'agustin@duitpropiedades.com.ar',
+    facebook: 'https://www.facebook.com/agustin.aznarez',
+    twitter: '',
+    linkedin: '',
+    instagram: '',
+    pic: '/images/team/agustin-aznarez-profile.jpg'
+  },
+  {
+    name: 'Martín Paradelo',
+    position: 'Asesor',
+    phone: '(351) 2463606',
+    email: 'mparadelo@duitpropiedades.com.ar',
+    facebook: 'https://www.facebook.com/martin.paradelo.7',
+    twitter: '',
+    linkedin: '',
+    instagram: 'https://www.instagram.com/martinparadelo/',
+    pic: '/images/team/martin-paradelo-profile.jpg'
+  },
+  {
+    name: 'Daniel Ganim',
+    position: 'Asesor',
+    phone: '(351) 5647780',
+    email: 'danielganim@duitpropiedades.com.ar',
+    facebook: ' ',
+    twitter: '',
+    linkedin: '',
+    instagram: '',
+    pic: '/images/team/daniel-ganim-profile.jpg'
+  },
+  {
+    name: 'Fernando Gavier',
+    position: 'Asesor',
+    phone: '(0351) 155145491',
+    email: 'fgavier@duitpropiedades.com.ar',
+    facebook: 'https://www.facebook.com/fngavier',
+    twitter: 'https://twitter.com/fngavier',
+    linkedin: '',
+    instagram: '',
+    pic: '/images/team/fernando-gavier-profile.jpg'
+  },
+  {
+    name: 'Hugo Miranda',
+    position: 'Asesor',
+    phone: '(351) 2462630',
+    email: 'hmiranda@duitpropiedades.com.ar',
+    facebook: ' ',
+    twitter: '',
+    linkedin: '',
+    instagram: '',
+    pic: '/images/team/hugo-miranda-profile.jpg'
+  },
+  {
+    name: 'Severo Sosa',
+    position: 'Asesor',
+    phone: '(351) 5648467',
+    email: 'severo@duitpropiedades.com.ar',
+    facebook: 'https://www.facebook.com/sapo.sosa',
+    twitter: 'https://twitter.com/severososa',
+    linkedin: 'https://www.linkedin.com/in/severo-sosa-barreneche-590a3b1b/',
+    instagram: 'http://www.instagram.com/sapososa/',
+    pic: '/images/team/severo-sosa-profile.jpg'
+  },
+  {
+    name: 'Cecilia Aznarez',
+    position: 'Asesora',
+    phone: '(351) 6573901',
+    email: 'cecilia@duitpropiedades.com.ar',
+    facebook: '',
+    twitter: '',
+    linkedin: '',
+    instagram: '',
+    pic: '/images/team/cecilia-aznarez-1-profile.jpg'
+  },
+  {
+    name: 'Carolina Valarolo',
+    position: 'Asesora',
+    phone: '(351) 5513711',
+    email: 'carolina@duitpropiedades.com.ar',
+    facebook: '',
+    twitter: '',
+    linkedin: '',
+    instagram: '',
+    pic: '/images/team/carolina-valarolo-1-profile.jpg'
+  },
+  {
+    name: 'Teresita Sciortino',
+    position: 'Secretaria Administración',
+    phone: '(351) 6840250',
+    email: 'teresita@duitpropiedades.com.ar',
+    facebook: 's',
+    twitter: '',
+    linkedin: '',
+    instagram: '',
+    pic: '/images/team/recepcion-profile.jpg'
+  }
+];
