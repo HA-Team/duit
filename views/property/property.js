0e7a16127813988a9353879d4ca3c3d7c8f9d842
LeviTamiozzo <levi.tzzo@gmail.com>
2020-04-17 11:27:03 -0300

    mobile-prop-detail: assessor contact location and mailSubject text corrected

--- views/property/property.js
+++ views/property/property.js
@@ -161,7 +161,8 @@ app.controller('property', function($rootScope, $scope, tokkoApi, $stateParams) 
       google.maps.event.trigger(mobileMap, 'resize');
     }, 0);
 
-    $scope.mailSubject = `Consulta%20por%20${$scope.p.prop.publication_title.replace(/\s/g, '%20')}`; 
+    const mailSubject = `Consulta por propiedad %23${$scope.p.id}: ${$scope.p.prop.publication_title}`
+    $scope.mailSubject = mailSubject.replace(/\s/g, '%20'); 
 
     const cellPhone = $scope.p.prop.producer.cellphone ? $scope.p.prop.producer.cellphone : $scope.p.prop.producer.phone;
     $scope.cleanCellPhone = `549${cellPhone.replace(/^0|\+|\-|\s/g, '')}`.replace(/^(54935115)/, '549351');     
