3fd8069bb9eb716aa17d12cc69d61cdd661ed87a
LeviTamiozzo <levi.tzzo@gmail.com>
2020-06-04 08:32:37 -0300

    Home: go to section update

--- views/home/home.js
+++ views/home/home.js
@@ -133,7 +133,7 @@ app.controller('homeSearch', function($rootScope, $scope, $state) {
   }
 });
 
-app.controller('home', function($scope) {
+app.controller('home', function($scope, $location, $anchorScroll) {
   const duitWhatsapp = '5493518172255';
   const duitPhone = '+5493518172255';
 
@@ -180,36 +180,41 @@ app.controller('home', function($scope) {
 
   $scope.services = [
     {
+      anchorName: "home-services-description-assesors",
       iconClass: "fas fa-building",
-      linkTo: "contactUs",
+      goTo: "home-contact",
       linkTitle: "Contactános",
       mainTitle: "Te aconsejamos",
       description: "El equipo profesional de Duit te asistirá en todo momento, bridandote todos los consejos necesarios para que tomes la decisión mas adecuada."
     },
     {
+      anchorName: "home-services-description-rent",
       iconClass: "far fa-building",
-      linkTo: "developments",
+      goTo: "home-search-bar",
       linkTitle: "Mas info",
       mainTitle: "Alquiler",
       description: "Seleccionamos los mejores emprendimientos y las mejores empresas desarrollistas del mercado para ponerlos a tu alcance."
     },
     {
+      anchorName: "home-services-description-sell",
       iconClass: "far fa-building",
-      linkTo: "developments",
+      goTo: "home-search-bar",
       linkTitle: "Mas info",
       mainTitle: "Venta",
       description: "Seleccionamos los mejores emprendimientos y las mejores empresas desarrollistas del mercado para ponerlos a tu alcance."
     },    
     {
+      anchorName: "home-services-description-admin",
       iconClass: "far fa-building",
-      linkTo: "developments",
+      goTo: "home-contact",
       linkTitle: "Mas info",
       mainTitle: "Administración",
       description: "Seleccionamos los mejores emprendimientos y las mejores empresas desarrollistas del mercado para ponerlos a tu alcance."
     },
     {
+      anchorName: "home-services-description-quotation",
       iconClass: "fas fa-home",
-      linkTo: "contactUs",
+      goTo: "home-contact",
       linkTitle: "Contactános",
       mainTitle: "Valuamos tu casa",
       description: "¿Querés saber cual es el valor de tu propiedad? Nuestro Staff de consejeros matriculados CPCPI valuarán profesionalmente tus bienes."
@@ -219,23 +224,23 @@ app.controller('home', function($scope) {
   $scope.servicesLinks = [
     {
       imgSrc: "/images/services/service-1.png",
-      goTo: "consejeros" 
+      goTo: "home-services-description-assesors" 
     },
     {
       imgSrc: "/images/services/service-2.png",
-      goTo: "searchBar" 
+      goTo: "home-services-description-rent" 
     },
     {
       imgSrc: "/images/services/service-3.png",
-      goTo: "searchBar" 
+      goTo: "home-services-description-sell" 
     },
     {
       imgSrc: "/images/services/service-4.png",
-      goTo: "contacto" 
+      goTo: "home-services-description-admin" 
     },
     {
       imgSrc: "/images/services/service-5.png",
-      goTo: "contacto" 
+      goTo: "home-services-description-quotation" 
     }
   ]
 
@@ -248,7 +253,4 @@ app.controller('home', function($scope) {
 
     else $anchorScroll();
   };
-
-  
-
 });
