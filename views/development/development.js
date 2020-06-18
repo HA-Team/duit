e5adb33da2b9c22c32b1bc1e1d43ee9572a31a3c
LeviTamiozzo <levi.tzzo@gmail.com>
2020-06-18 20:28:07 -0300

    Refactor GetFeatured into service

--- views/development/development.js
+++ views/development/development.js
@@ -1,57 +1,4 @@
-// const getFeatured = (scope, tokkoApi) => {
-//   scope.featured = [];
-//   scope.featReady = false;
-//   let data = tokkoSearchArgs.data;
-//   data.filters.push(["is_starred_on_web", "=", "true"]);
-//   const args = {data: JSON.stringify(data), order: 'desc'}
-//   tokkoApi.find('property/search', args, function(result){
-//     let props = [];
-//     result.forEach((p) => {
-//       props.push({
-//         id: p.id,
-//         type: p.operations[0].operation_type,
-//         currency: p.operations[p.operations.length-1].prices.slice(-1)[0].currency,
-//         price: p.operations[p.operations.length-1].prices.slice(-1)[0].price,
-//         cover_photo: p.photos[0].thumb,
-//         parkings: p.parking_lot_amount ? p.parking_lot_amount : 0,
-//         prop: p
-//       })
-//     });
-//     scope.featured = props;
-//     scope.featReady = true;
-//     scope.$apply();
-//     uiFunctions.buildCarousel();
-//   });  
-// };
-
-const getDevProps = (scope, tokkoApi) => {
-  scope.devProps = [];
-  scope.devPropsReady = false;
-  const args = {development: scope.d.id, order: 'desc'};
-  tokkoApi.find('property', args, function(result){
-    let props = [];
-    result.forEach((p) => {
-      props.push({
-        id: p.id,
-        type: p.operations[0].operation_type,
-        currency: p.operations[p.operations.length-1].prices.slice(-1)[0].currency,
-        price: p.operations[p.operations.length-1].prices.slice(-1)[0].price,
-        cover_photo: p.photos[0].image,
-        parkings: p.parking_lot_amount ? p.parking_lot_amount : 0,
-        area: p.type.id === 1 ? p.surface : p.roofed_surface,
-        sell: p.operations.filter(p => p.operation_type == "Venta")[0]?.prices.slice(-1)[0],
-        rent: p.operations.filter(p => p.operation_type == "Alquiler")[0]?.prices.slice(-1)[0],
-        parkings_av: p.parking_lot_amount > 0 ? "Si" : "No",
-        prop: p
-      })
-    });
-    scope.devProps = props;
-    scope.devPropsReady = true;
-    scope.$apply();
-  });  
-};
-
-app.controller('development', function($rootScope, $scope, tokkoApi, $stateParams) {
+app.controller('development', function($rootScope, $scope, tokkoApi, $stateParams, getFeaturedProperties) {
   $rootScope.activeMenu = '';
   $scope.apiReady = false;
   const id = $stateParams.devId;
@@ -72,8 +19,8 @@ app.controller('development', function($rootScope, $scope, tokkoApi, $stateParam
     uiFunctions.showMoreButton();
     uiFunctions.buildSlickCarousel();
     uiFunctions.buildMagnificPopup();
-    getFeatured($scope, tokkoApi);
-    getDevProps($scope, tokkoApi);
+    getFeaturedProperties.getFeatured($scope, tokkoApi);
+    getFeaturedProperties.getDevProps($scope, tokkoApi);
     setTimeout(() => {
       google.maps.event.trigger(map, 'resize');
     }, 0)
--- views/property/property.js
+++ views/property/property.js
@@ -1,61 +1,3 @@
-const getFeatured = (scope, tokkoApi) => {
-  scope.featured = [];
-  scope.featReady = false;
-  let data = tokkoSearchArgs.data;
-  data.filters.push(["is_starred_on_web", "=", "true"]);
-  const args = {data: JSON.stringify(data), order: 'desc'}
-  tokkoApi.find('property/search', args, function(result){
-    let props = [];
-    result.forEach((p) => {
-      props.push({
-        id: p.id,
-        type: p.operations[0].operation_type,
-        currency: p.operations[p.operations.length-1].prices.slice(-1)[0].currency,
-        price: p.operations[p.operations.length-1].prices.slice(-1)[0].price,
-        cover_photo: p.photos[0].image,
-        parkings: p.parking_lot_amount ? p.parking_lot_amount : 0,
-        area: p.type.id === 1 ? p.surface : p.roofed_surface,
-        prop: p
-      })
-    });
-    scope.featured = props;
-    scope.featReady = true;
-    scope.$apply();
-    uiFunctions.buildCarousel();
-  });  
-};
-
-const getSimilar = (scope, tokkoApi) => {
-  scope.similar = [];
-  scope.simReady = false;
-  let data = tokkoSearchArgs.data;
-  data.operation_types = scope.p.operation_type == 'Venta' ? [1] : [2];
-  data.property_types = [scope.p.prop.type.id];
-  data.filters = [];
-  if(scope.p.prop.custom_tags.length > 0) {
-    data.with_custom_tags = [scope.p.prop.custom_tags.slice(-1)[0].id];
-  }
-  const args = {data: JSON.stringify(data), order: 'desc'};
-  tokkoApi.find('property/search', args, function(result){
-    let props = [];
-    result.forEach((p) => {
-      props.push({
-        id: p.id,
-        type: p.operations[0].operation_type,
-        currency: p.operations[p.operations.length-1].prices.slice(-1)[0].currency,
-        price: p.operations[p.operations.length-1].prices.slice(-1)[0].price,
-        cover_photo: p.photos[0] ? p.photos[0].image : '/images/no-image.png',
-        parkings: p.parking_lot_amount ? p.parking_lot_amount : 0,
-        area: p.type.id === 1 ? p.surface : p.roofed_surface,
-        prop: p
-      })
-    });
-    scope.similar = props;
-    scope.simReady = true;
-    scope.$apply();
-  });  
-};
-
 const swipeSlider = function(e, slider, scope, length, currentNumberName, side) {
   e.stopPropagation();
   
@@ -64,7 +6,7 @@ const swipeSlider = function(e, slider, scope, length, currentNumberName, side) 
   scope.moveSlider(slider, length, currentNumberName, side, pxToMove);
 };
 
-app.controller('property', function($rootScope, $scope, tokkoApi, $stateParams) {      
+app.controller('property', function($rootScope, $scope, tokkoApi, $stateParams, getFeaturedProperties) {      
   $rootScope.activeMenu = '';
   $scope.apiReady = false;
   $scope.isContactModalOpen = false;
@@ -162,8 +104,8 @@ app.controller('property', function($rootScope, $scope, tokkoApi, $stateParams) 
     uiFunctions.showMoreButton();
     uiFunctions.buildSlickCarousel();
     uiFunctions.buildMagnificPopup();
-    getFeatured($scope, tokkoApi);
-    getSimilar($scope, tokkoApi);
+    getFeaturedProperties.getFeatured($scope, tokkoApi);
+    getFeaturedProperties.getSimilar($scope, tokkoApi);
     setTimeout(() => {
       google.maps.event.trigger(map, 'resize');
       google.maps.event.trigger(mobileMap, 'resize');
