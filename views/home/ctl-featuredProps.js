d78da9cf5cabd2e5cd77aeda6ab7b96c8e5df3ec
LeviTamiozzo <levi.tzzo@gmail.com>
2020-06-18 14:15:10 -0300

    Refactor home into controllers and tokkoApi into service

---
+++ views/home/ctl-featuredProps.js
@@ -0,0 +1,35 @@
+app.controller('featuredProps', function($scope, tokkoApi) {
+  $scope.featured = [];
+  $scope.featuredPropsReady = false;
+  $scope.duit360PropsReady = false;
+
+  let data = JSON.parse(_.clone(tokkoSearchArgs.sData));
+  data.filters.push(["is_starred_on_web", "=", "true"]);
+  let args = {data: JSON.stringify(data), order: 'desc'};
+  
+  tokkoApi.find('property/search', args, function(result){
+    let props = [];
+    
+    result.forEach((p) => {
+      props.push({
+        id: p.id,
+        title: p.publication_title,
+        area: p.type.id === 1 ? p.surface : p.roofed_surface,
+        type: p.operations[0].operation_type,
+        currency: p.operations[p.operations.length-1].prices.slice(-1)[0].currency,
+        price: p.operations[p.operations.length-1].prices.slice(-1)[0].price,
+        rooms: p.suite_amount,
+        baths: p.bathroom_amount,
+        parkings: p.parking_lot_amount,
+        cover_photo: p.photos[0].image,
+        prop: p,
+      })
+    });
+
+    $scope.featured = props;
+    $scope.featuredPropsReady = true;
+    $scope.$apply();
+    uiFunctions.buildCarousel();
+    $(window).trigger('resize');
+  });
+});
