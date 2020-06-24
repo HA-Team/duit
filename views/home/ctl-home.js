15b5b469c4aaebbe6342e4db96cb0154a8bbf771
LeviTamiozzo <levi.tzzo@gmail.com>
2020-06-24 00:28:40 -0300

    Home: Update Layout and Services

--- views/home/ctl-home.js
+++ views/home/ctl-home.js
@@ -45,74 +45,28 @@ app.controller('home', function($scope, $location, $anchorScroll, anchorSmoothSc
 
   $scope.services = [
     {
-      imgSrc: "/images/services/iso-service-1.png",
-      anchorName: "home-services-description-assesors",
-      iconClass: "fas fa-building",
-      goTo: "home-contact",
-      linkTitle: "Contactános",
-      mainTitle: "Te aconsejamos",
-      description: "El equipo profesional de Duit te asistirá en todo momento, bridandote todos los consejos necesarios para que tomes la decisión mas adecuada."
+      imgSrc: "/images/services/consejeros.png",
+      goTo: "home-assesors" 
     },
     {
-      imgSrc: "/images/services/iso-service-2.png",
-      anchorName: "home-services-description-rent",
-      iconClass: "far fa-building",
+      imgSrc: "/images/services/alquiler.png",
       goTo: "home-search-bar",
-      linkTitle: "Mas info",
-      mainTitle: "Alquiler",
-      description: "Seleccionamos los mejores emprendimientos y las mejores empresas desarrollistas del mercado para ponerlos a tu alcance."
+      action: () => $scope.$broadcast('homeSearchServiceLinkClicked', { type: 2})
     },
     {
-      imgSrc: "/images/services/iso-service-3.png",
-      anchorName: "home-services-description-sell",
-      iconClass: "far fa-building",
+      imgSrc: "/images/services/venta.png",
       goTo: "home-search-bar",
-      linkTitle: "Mas info",
-      mainTitle: "Venta",
-      description: "Seleccionamos los mejores emprendimientos y las mejores empresas desarrollistas del mercado para ponerlos a tu alcance."
-    },    
-    {
-      imgSrc: "/images/services/iso-service-4.png",
-      anchorName: "home-services-description-admin",
-      iconClass: "far fa-building",
-      goTo: "home-contact",
-      linkTitle: "Mas info",
-      mainTitle: "Administración",
-      description: "Seleccionamos los mejores emprendimientos y las mejores empresas desarrollistas del mercado para ponerlos a tu alcance."
+      action: () => $scope.$broadcast('homeSearchServiceLinkClicked', { type: 1}) 
     },
     {
-      imgSrc: "/images/services/iso-service-5.png",
-      anchorName: "home-services-description-quotation",
-      iconClass: "fas fa-home",
-      goTo: "home-contact",
-      linkTitle: "Contactános",
-      mainTitle: "Valuamos tu casa",
-      description: "¿Querés saber cual es el valor de tu propiedad? Nuestro Staff de consejeros matriculados CPCPI valuarán profesionalmente tus bienes."
-    }        
-  ];
-
-  $scope.servicesLinks = [
-    {
-      imgSrc: "/images/services/service-1.png",
-      goTo: "home-services-description-assesors" 
+      imgSrc: "/images/services/administracion.png",
+      goTo: "home-contact" 
     },
     {
-      imgSrc: "/images/services/service-2.png",
-      goTo: "home-services-description-rent" 
-    },
-    {
-      imgSrc: "/images/services/service-3.png",
-      goTo: "home-services-description-sell" 
-    },
-    {
-      imgSrc: "/images/services/service-4.png",
-      goTo: "home-services-description-admin" 
-    },
-    {
-      imgSrc: "/images/services/service-5.png",
-      goTo: "home-services-description-quotation" 
+      imgSrc: "/images/services/tasacion.png",
+      goTo: "home-contact" 
     }
-  ]
+  ];
 
   $scope.goToSection = (id) => { 
     var newHash = id;    
--- views/home/ctl-homeSearch.js
+++ views/home/ctl-homeSearch.js
@@ -1,98 +1,109 @@
 app.controller('homeSearch', function($rootScope, $scope, $state) {
-    $rootScope.activeMenu = 'home';
-    $scope.operationType = [2];
-    $scope.subTypes = [];  
-  
+  $rootScope.activeMenu = 'home';
+  $scope.operationType = [2];
+  $scope.subTypes = [];
+
+  $scope.isPropertyTypeOpen = false;
+  $scope.isSubPropertyTypeOpen = false;
+
+  $scope.wasFindPropertiesButtonClicked = false;
+  $scope.isPropertyPlaceholderWarningActive = false;
+
+  $scope.subTypeSelected = {
+    id: -1,
+    name:"Condición"
+  };
+
+  $scope.propertyType = {
+    id: -1,
+    name: "Tipo de Propiedad"
+  };
+
+  $scope.propertiesTypes = propertiesTypes;
+
+  $scope.updateTypeChosen = function(newType) { 
+    $scope.propertyType = newType;
+
     $scope.isPropertyTypeOpen = false;
     $scope.isSubPropertyTypeOpen = false;
-  
-    $scope.wasFindPropertiesButtonClicked = false;
-    $scope.isPropertyPlaceholderWarningActive = false;  
-  
-    $scope.subTypeSelected = {
-      id: -1,
-      name:"Condición"
-    };
-  
-    $scope.propertyType = {
-      id: -1,
-      name: "Tipo de Propiedad"
-    };
-  
-    $scope.propertiesTypes = propertiesTypes;    
-  
-    $scope.updateTypeChosen = function(newType) {  
-      $scope.propertyType = newType;
-  
-      $scope.isPropertyTypeOpen = false;
-      $scope.isSubPropertyTypeOpen = false;
-      $scope.isPropertyPlaceholderWarningActive = false;
-  
-      if ($scope.propertiesTypes.some(type => type.id == -1) && $scope.propertiesTypes[0] != -1) $scope.propertiesTypes.shift();            
-      
-      $scope.subTypeSelected = propertiesSubTypes[$scope.propertyType.id][0]; 
-      
-      $scope.subTypes = propertiesSubTypes[$scope.propertyType.id].filter(type => type.id != $scope.subTypeSelected.id);
-      
-      $scope.propertiesTypes = propertiesTypes.filter(type => type.id != newType.id); 
-    };
-  
-    $scope.updateSubTypeChosen = (newType) => {
-      $scope.subTypeSelected = newType;
-  
-      $scope.isPropertyTypeOpen = false;
-      $scope.isSubPropertyTypeOpen = false;
-  
-      $scope.subTypes = propertiesSubTypes[$scope.propertyType.id].filter(type => type.id != $scope.subTypeSelected.id);
-    };
-  
-    $scope.togglePropertyTypeDropdown = () => {    
-      $scope.isSubPropertyTypeOpen = false;
-      $scope.isPropertyTypeOpen = !$scope.isPropertyTypeOpen;
-    };
-  
-    $scope.toggleSubPropertyTypeDropdown = () => {
-      $scope.isPropertyTypeOpen = false;
-      if ($scope.propertyType.id != -1 && $scope.subTypes.length > 1) $scope.isSubPropertyTypeOpen = !$scope.isSubPropertyTypeOpen;
-    };
-  
-    $scope.closeOpenSelects = () => {
-      $scope.isPropertyTypeOpen = false;
-      $scope.isSubPropertyTypeOpen = false;
-    };
-  
-    $scope.findProperties = () => {
-      $scope.wasFindPropertiesButtonClicked = true;
-  
-      if ($scope.propertyType.id == -1) {
-        $scope.isPropertyPlaceholderWarningActive = true;
-  
-        setTimeout(() => {
-          $scope.isPropertyPlaceholderWarningActive = false;
-          $scope.$apply();
-        }, 2000);
-      }
-      else {
-        $scope.isPropertyPlaceHolderWarningActive = false;
-        $scope.find();
-      }
+    $scope.isPropertyPlaceholderWarningActive = false;
+
+    if ($scope.propertiesTypes.some(type => type.id == -1) && $scope.propertiesTypes[0] != -1) $scope.propertiesTypes.shift();
+    
+    $scope.subTypeSelected = propertiesSubTypes[$scope.propertyType.id][0];
+    
+    $scope.subTypes = propertiesSubTypes[$scope.propertyType.id].filter(type => type.id != $scope.subTypeSelected.id);
+    
+    $scope.propertiesTypes = propertiesTypes.filter(type => type.id != newType.id);
+  };
+
+  $scope.updateSubTypeChosen = (newType) => {
+    $scope.subTypeSelected = newType;
+
+    $scope.isPropertyTypeOpen = false;
+    $scope.isSubPropertyTypeOpen = false;
+
+    $scope.subTypes = propertiesSubTypes[$scope.propertyType.id].filter(type => type.id != $scope.subTypeSelected.id);
+  };
+
+  $scope.togglePropertyTypeDropdown = () => { 
+    $scope.isSubPropertyTypeOpen = false;
+    $scope.isPropertyTypeOpen = !$scope.isPropertyTypeOpen;
+  };
+
+  $scope.toggleSubPropertyTypeDropdown = () => {
+    $scope.isPropertyTypeOpen = false;
+    if ($scope.propertyType.id != -1 && $scope.subTypes.length > 1) $scope.isSubPropertyTypeOpen = !$scope.isSubPropertyTypeOpen;
+  };
+
+  $scope.closeOpenSelects = () => {
+    $scope.isPropertyTypeOpen = false;
+    $scope.isSubPropertyTypeOpen = false;
+  };
+
+  $scope.findProperties = () => {
+    $scope.wasFindPropertiesButtonClicked = true;
+
+    if ($scope.propertyType.id == -1) {
+      $scope.isPropertyPlaceholderWarningActive = true;
+
+      setTimeout(() => {
+        $scope.isPropertyPlaceholderWarningActive = false;
+        $scope.$apply();
+      }, 2000);
     }
-  
-    setTimeout(function(){
-      uiFunctions.buildParallax();
-      uiFunctions.buildChosen();
-      uiFunctions.buildSearchTypeButtons();
-      uiFunctions.buildBackToTop();
-    }, 0);
-  
-    $scope.find = () => {
-      let data = JSON.parse(_.clone(tokkoSearchArgs.sData));
-      data.operation_types = [$scope.operationType[0]];    
-      if ($scope.propertyType) data.property_types = [$scope.propertyType.id];
-      if ($scope.subTypeSelected) {
-        data.with_custom_tags = $scope.subTypeSelected.id == 0 ? [] : [$scope.subTypeSelected.id];
-      }
-      let args = {data: JSON.stringify(data), offset: 0};
-      $state.go('propertySearch', {args: JSON.stringify(args)});
-    };
+    else {
+      $scope.isPropertyPlaceHolderWarningActive = false;
+      $scope.find();
+    }
+  }
+
+  setTimeout(function(){
+    uiFunctions.buildParallax();
+    uiFunctions.buildChosen();
+    uiFunctions.buildSearchTypeButtons();
+    uiFunctions.buildBackToTop();
+  }, 0);
+
+  $scope.find = () => {
+    let data = JSON.parse(_.clone(tokkoSearchArgs.sData));
+    data.operation_types = [$scope.operationType[0]];
+    if ($scope.propertyType) data.property_types = [$scope.propertyType.id];
+    if ($scope.subTypeSelected) {
+      data.with_custom_tags = $scope.subTypeSelected.id == 0 ? [] : [$scope.subTypeSelected.id];
+    }
+    let args = {data: JSON.stringify(data), offset: 0};
+    $state.go('propertySearch', {args: JSON.stringify(args)});
+  };
+
+  $scope.$on('homeSearchServiceLinkClicked', (event, {type}) => {
+    const button = document.querySelector(`#home-search-bar .search-type input[value='${type}']`).parentElement;    
+
+    const notClickedButtons = document.querySelectorAll(`#home-search-bar .search-type input:not([value='${type}'])`);
+    
+    button.classList.add("active");
+    notClickedButtons.forEach(element => element.parentElement.classList.remove("active"));
+
+    $scope.operationType = [type];
   });
+});
