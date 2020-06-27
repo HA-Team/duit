50f651f040b05ef2a94e0643e62c8f88f193b877
LeviTamiozzo <levi.tzzo@gmail.com>
2020-06-27 02:27:31 -0300

    Update Development listing and detail

--- views/property/ctl-propContactForm.js
+++ views/property/ctl-propContactForm.js
@@ -1,45 +1,45 @@
-app.controller('propContactForm', function($scope, tokkoApi) {
-    $scope.submitText = 'Enviar';
-    $scope.sending = false;
-    $scope.success = false;
-    $scope.error = false;
-    $scope.send = function () {
-        if ($scope.name || $scope.email) {
-            $scope.submitText = 'Enviando';
-            $scope.sending = true;
+app.controller('propContactForm', function ($scope, tokkoApi) {
+	$scope.submitText = 'Enviar';
+	$scope.sending = false;
+	$scope.success = false;
+	$scope.error = false;
+	$scope.send = function () {
+		if ($scope.name || $scope.email) {
+			$scope.submitText = 'Enviando';
+			$scope.sending = true;
 
-            const data = {
-                email: $scope.email,
-                phone: $scope.phone,
-                text: $scope.message,
-                properties: [$scope.p.id],
-            };
+			const data = {
+				email: $scope.email,
+				phone: $scope.phone,
+				text: $scope.message,
+				properties: [$scope.p.id],
+			};
 
-            tokkoApi.insert('webcontact', data, function (response) {
-                if (response.result=='success') {
-                    $scope.sending = false;
-                    $scope.submitText = 'Enviar';
-                    $scope.success = true;
-                    $scope.email = '';
-                    $scope.phone = '';
-                    $scope.message = '';
+			tokkoApi.insert('webcontact', data, function (response) {
+				if (response.result == 'success') {
+					$scope.sending = false;
+					$scope.submitText = 'Enviar';
+					$scope.success = true;
+					$scope.email = '';
+					$scope.phone = '';
+					$scope.message = '';
 
-                    setTimeout(function() {
-                        $scope.success = false;
-                        $scope.$apply()
-                    }, 3000);
+					setTimeout(function () {
+						$scope.success = false;
+						$scope.$apply();
+					}, 3000);
 
-                    $scope.$apply();
-                } else {
-                    $scope.error = true;
-                    $scope.$apply();
+					$scope.$apply();
+				} else {
+					$scope.error = true;
+					$scope.$apply();
 
-                    setTimeout(function() {
-                        $scope.error = false;
-                        $scope.$apply()
-                    }, 3000);
-                }
-            });
-        }
-    };
+					setTimeout(function () {
+						$scope.error = false;
+						$scope.$apply();
+					}, 3000);
+				}
+			});
+		}
+	};
 });
