866198f9ed42e52e2941360cff38d9d6e4d87d8e
LeviTamiozzo <levi.tzzo@gmail.com>
2020-08-07 16:44:14 -0300

    Revert "change paging to overflow auto"
    This reverts commit dff692310c032bf7045cc4a89ed050a4e498f396.

--- views/components/responsive-table/responsive-table.js
+++ views/components/responsive-table/responsive-table.js
@@ -17,8 +17,52 @@ app.directive('responsiveTable', function() {
 
             // #region Private Properties
 
+            const tableContainer = $element.find('.responsive-table-container')[0];
             const tableSlider = $element.find('.responsive-table-slider')[0];
             const tableFixed = $element.find('.responsive-table-fixed')[0];
+            const setedMarginsWidth = 20;
+            
+            let pagingSteps = [0];
+
+            // #endregion
+
+            // #region Private Methods
+
+            const moveSlider = (side) => {
+                let pxToMove;
+                if (side == 'right') pxToMove = pagingSteps[$scope.currentIndex];
+                else pxToMove = $scope.currentIndex == 1 ? pagingSteps[$scope.pages - 1] : pagingSteps[$scope.currentIndex - 2];
+
+                $scope.currentIndex = sliderMoves.moveSliderByScrollLeft(tableSlider, $scope.currentIndex, $scope.pages, side, pxToMove);
+                $scope.$apply();
+            }
+
+            const setPaging = (slider, fixed) => {
+                const spareWidth = window.innerWidth - setedMarginsWidth - fixed.offsetWidth;
+                const columns = [...slider.querySelectorAll('.responsive-table-column')];
+
+                $scope.pages = 1;
+
+                let widthSum = 0;
+
+                columns.forEach(col => {
+                    if (widthSum + col.offsetWidth <= spareWidth) widthSum += col.offsetWidth;
+                    else {
+                        if ($scope.pages == 1) {
+                            pagingSteps.push(widthSum);
+                            widthSum = col.offsetWidth;
+                            $scope.pages ++;
+                        } else {
+                            pagingSteps.push(widthSum + pagingSteps[$scope.pages - 1]);
+                            widthSum = col.offsetWidth;
+                            $scope.pages ++;
+                        }
+                    }
+                });
+
+                $scope.totalPages = [...Array(parseInt($scope.pages)).keys()];
+                $scope.$apply();
+            };
 
             // #endregion
 
@@ -47,6 +91,23 @@ app.directive('responsiveTable', function() {
                 }
                 else navigation.goToSection('property', '', null, { propertyId: item.id });
             };
+
+            // #endregion
+
+            // #region Events
+
+            setTimeout(() => setPaging(tableSlider, tableFixed), 0);
+
+            utils.sides.forEach(side => {
+                const sliderArrow = $element.find(`.responsive-table-page-counter .fa-angle-${side.side}`)[0];
+
+                sliderArrow.addEventListener("click", () => moveSlider(side.side)); 
+
+                tableContainer.addEventListener(`swiped-${side.side}`, (e) => {
+                    e.preventDefault();
+                    moveSlider(side.oposite);
+                }, {capture: true});
+            });
 
             // #endregion
         }]            
