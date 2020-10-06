b60ca2e87ca38a82176b63a6294854c9f06e0ae5
LeviTamiozzo <levi.tzzo@gmail.com>
2020-10-06 13:27:47 -0300

    Add home services texts + cache update

--- views/home/ctl-home.js
+++ views/home/ctl-home.js
@@ -153,14 +153,14 @@ app.controller('homeController', ['$rootScope', '$scope', '$interval', '$timeout
     {
       imgSrc: "/images/services/administracion.png",
       hasTooltip: true,
-      tooltipText: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+      tooltipText: "Realizamos el cobro de alquilers e impuestos, y mantenimiento de tu propiedad. Damos una respuesta a los locatarios y evitamos a los propietarios las complicaciones derivadas con la administración y gestión de propiedades.",
       whatsAppHRef: `https://api.whatsapp.com/send?phone=${sharedData.infoWhatsapp}&text=${window.encodeURIComponent('Hola, quiero hacer una consulta por una administración.')}`,
       emailHRef: `mailto:${sharedData.infoEmail}?Subject=${window.encodeURIComponent('Hola, quiero hacer una consulta por una administración.')}`
     },
     {
       imgSrc: "/images/services/tasacion.png",
       hasTooltip: true,
-      tooltipText: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      tooltipText: "Nuestro asesores matriculados CPCPI valuarán tu inmueble, para que sepas cuanto vale realemente tu propiedad.",
       whatsAppHRef: `https://api.whatsapp.com/send?phone=${sharedData.infoWhatsapp}&text=${window.encodeURIComponent('Hola, quiero hacer una consulta por una tasasión.')}`,
       emailHRef: `mailto:${sharedData.infoEmail}?Subject=${window.encodeURIComponent('Hola, quiero hacer una consulta por una tasasión.')}`
     }
